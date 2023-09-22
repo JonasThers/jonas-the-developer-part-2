f3d753267a64f1d9d2c8487c314f5ca30216b29e
JonasThers <jonasthers1992@gmail.com>
2023-09-22 17:17:22 +0200

    Add CRUD functionality for degrees

--- app/controllers/degrees_controller.rb
+++ app/controllers/degrees_controller.rb
@@ -1,4 +1,38 @@
 class DegreesController < ApplicationController
+
   def index
+    degrees = Degree.all
+
+    render json: degrees
+  end
+
+  def show
+    degree = Degree.find(params[:id])
+
+    render json: degree
+  end
+
+  def create
+    degree = Degree.create!(degree_params)
+
+    render json: degree, status: :created
+  end
+
+  def update
+    degree = Degree.find(params[:id])
+    degree.update!(degree_params)
+    
+    render json: degree
+  end
+
+  def destroy
+    Degree.find(params[:id]).destroy
+    head :no_content
+  end
+  
+  private
+  
+  def degree_params
+    params.permit(:title, :university)
   end
 end
