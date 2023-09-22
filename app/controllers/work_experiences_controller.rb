42d14d7c6f34163eccce6b7b1622b6e204a40c50
JonasThers <jonasthers1992@gmail.com>
2023-09-22 17:07:58 +0200

    Add CRUD functionality for work experience

--- app/controllers/work_experiences_controller.rb
+++ app/controllers/work_experiences_controller.rb
@@ -1,4 +1,36 @@
 class WorkExperiencesController < ApplicationController
+
   def index
+    work_experiences = WorkExperience.all
+
+    render json: work_experiences
+  end
+
+  def show
+    work_experience = WorkExperience.find(params[:id])
+
+    render json: work_experience
+  end
+
+  def create
+    work_experience = WorkExperience.create!(work_experience_params)
+    render json: work_experience, status: :created
+  end
+
+  def update
+    work_experience = WorkExperience.find(params[:id])
+    work_experience.update!(work_experience_params)
+    render json: work_experience
+  end
+
+  def destroy
+    WorkExperience.find(params[:id]).destroy
+    head :no_content
+  end
+  
+  private
+  
+  def work_experience_params
+    params.permit(:title, :workplace)
   end
 end
