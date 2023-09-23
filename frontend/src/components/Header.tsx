13879e8b52e0d9fa7c9b581db4b159226a889eec
JonasThers <jonasthers1992@gmail.com>
2023-09-23 12:50:36 +0200

    Add current workplace to header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,6 +2,7 @@ export const Header: React.FC = () => {
   return (
     <header>
       <h1>Jonas The Developer</h1>
+      <h5>Currently employed at <a href="https://venuu.com/" target="_blank">Venuu</a></h5>
     </header>
   );
 };
